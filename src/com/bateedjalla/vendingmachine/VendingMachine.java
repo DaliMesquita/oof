27f37e4eb72ccbf4b7bc3f59e548fc7d51e0f3e8
dalim <26018@ma-web.nl>
2018-09-27 12:41:21 +0200

    first commit

---
+++ src/com/bateedjalla/vendingmachine/VendingMachine.java
@@ -0,0 +1,13 @@
+package com.bateedjalla.vendingmachine;
+
+public class VendingMachine {
+
+
+    public static void main (String[] args ) {
+
+        String question = "wil je iets eten?";
+
+
+
+    }
+}
